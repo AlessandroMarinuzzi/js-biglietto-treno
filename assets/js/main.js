51cc0693ed26e27068d577b95199933427f8f258
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-23 13:55:31 +0100

    Init Commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+// Numero di Km che il passeggero vuole percorrere
+var Km = prompt("How many km does the passenger want to travel for?")
+console.log(Km);
+// Età del passeggero
+var Age = prompt("How old the passenger is?")
+console.log(Age);
