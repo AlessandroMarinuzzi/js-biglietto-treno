06d175b321b2c90761acc81a9d3747a48d16c3d5
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-23 15:49:24 +0100

    Final Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 
 // Numero di Km che il passeggero vuole percorrere
-var Km = prompt("How many km does the passenger want to travel for?");
+var Km = parseInt(prompt("How many km does the passenger want to travel for?"));
 console.log(Km);
+
+if (isNaN(Km)){
+    alert("Insert only numbers!")
+}
+
 // Età del passeggero
-var Age = prompt("How old the passenger is?");
+var Age = parseInt(prompt("How old the passenger is?"));
 console.log(Age);
+
+if (isNaN(Age)){
+    alert("Insert only numbers!")
+}
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -14,11 +23,13 @@ console.log(ticketPrice);
 
 var Price = ticketPrice*Km;
 console.log(Price);
-// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 20% per i minorenni e va applicato uno sconto del 40% per gli over 65.
 if (Age<18){
     Price = Price - (Price*(20/100));
     console.log(Price);
-} if else (Age>=65) {
-    Price = Price
-}
-// va applicato uno sconto del 40% per gli over 65.
+} else if (Age>=65) {
+    Price = Price - (Price*(40/100))
+    console.log(Price);
+} 
+// Messaggio finale circa il prezzo del biglietto.
+document.getElementById("cost").innerHTML = "Il prezzo del tuo biglietto è di £" + Price.toFixed(2)
